663ad2b773801fe7909d7bf16e23e12207abbb5c
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-16 12:26:03 +0530

    completed project

--- src/Components/Todolist.jsx
+++ src/Components/Todolist.jsx
@@ -1,37 +1,90 @@
-import {React, useState, useEffect} from 'react';
+import { React, useState, useEffect } from 'react';
 import "../styles/todolist.css";
-import {connect} from 'react-redux';
-import { type } from '@testing-library/user-event/dist/type';
+import { connect } from 'react-redux';
+import {v4 as uuidv4} from 'uuid';
+import bgImg from '../assets/backgroundimg.jpg';
+import bgImg2 from '../assets/bd3.jpg';
 
 function Todolist(store) {
-  const [newTodo, setNewTodo] = useState('')
-  function getTodoValue (e){
-    setNewTodo(e.target.value)
-    console.log(newTodo)
+  const [newTodo, setNewTodo] = useState({ title: '', status: false, id:''});
+  const [filteredTodos, setfiltfilteredTodos] = useState(store.todolist.todos);
+  const [filterKey, setfilterKey] = useState('all');
+
+  function getTodoValue(e) {
+    setNewTodo({ ...newTodo, title:e.target.value, id:uuidv4()})
   }
 
+  function todofilter (e){
+    setfilterKey(e.target.value)
+  }
+
+  useEffect(() => {
+    if(filterKey === 'all'){
+      setfiltfilteredTodos(store.todolist.todos)
+    }
+
+    if(filterKey === 'completed'){
+      var temp = store.todolist.todos.filter((todo)=>{
+        return todo.status == true
+      })
+      setfiltfilteredTodos([...temp])
+    }
+    
+    if(filterKey === 'notCompleted'){
+      var temp = store.todolist.todos.filter((todo)=>{
+        return todo.status == false
+      })
+      setfiltfilteredTodos([...temp])
+    }
+
+  }, [store.todolist, filterKey])
+
   return (
-    <div className='container todolist'>
-      <h1>{`Todos(${store.todolist.todos.length})`}</h1>
-       <ul className='ul-tag'>
+    <div className='todo-advance'>
+      <div className='bg-img-div'>
+       <img src={bgImg2} alt="loading" />  
+      </div>
+      <div className='container todolist'>
+      <h1> <span>{`plan your day`}</span> </h1>
+      <h3>{`total tasks(${store.todolist.todos.length})`}</h3>
+      <ul className='ul-tag'>
         <li className='input-todo'>
-          <input type="text" placeholder='enter todo' onChange={getTodoValue}/>
-          <button className='add-todo' onClick={()=>{store.dispatch({type:'ADDTODO', payload:newTodo})}}>Add todo</button>
+          <input type="text" placeholder='create your own task' onChange={getTodoValue} />
+          <button className='add-todo' onClick={() => { store.dispatch({ type: 'ADDTODO', payload: newTodo }) }}>Add todo</button>
         </li>
-        {store.todolist.todos.map((todo, index)=>{
+        <li className='filter-todos'>
+          <div>
+            <input type="radio" name='filter' value='all' onClick={todofilter}/>
+            <span>all</span>
+          </div>
+          <div>
+            <input type="radio" name='filter' value='completed' onClick={todofilter}/>
+            <span>completed</span>
+          </div>
+          <div>
+            <input type="radio" name='filter' value='notCompleted' onClick={todofilter}/>
+            <span>not completed</span>
+          </div>
+        </li>
+        { filteredTodos.map((todo, index) => {
           return (
-            <li className='todo'>
-              <h3>{`${index+1}. ${todo}`}</h3>
+            <li className={`todo ${todo.status ? 'todo-completed' : 'todo-not-completed'}`} data-id={todo.id}>
+              <span>{`${index + 1}. ${todo.title} - ${todo.status ? 'completed' : 'not completed'}`}</span>
               <div>
-              <button className='done-todo' onClick={()=>{store.dispatch({type:'DONETODO', payload:index})}}>Done</button>
-              <button className='delete-todo' onClick={()=>{store.dispatch({type:'DELETETODO', payload:index})}}>Delete</button>
+                { todo.status ?
+                <button className='undoTodo todo-btn' onClick={() => { store.dispatch({ type:'TOGGLE_TODO_STATUS', payload:todo}) }}>undo</button> :
+                <button className='done-todo todo-btn' onClick={() => { store.dispatch({ type:'TOGGLE_TODO_STATUS', payload:todo}) }}>Done</button>
+                }
+                <button className='delete-todo todo-btn' onClick={() => { store.dispatch({ type: 'DELETETODO', payload: todo }) }}>Delete</button>
               </div>
-              </li>
+            </li>
           )
         })}
-       </ul>
+      </ul>
     </div>
+    </div>
+    
   )
 }
 
-export default connect((store)=>{return store})(Todolist);
+export default connect((store) => { return store })(Todolist);
--- src/store/reducers/todoReducers.js
+++ src/store/reducers/todoReducers.js
@@ -1,21 +1,57 @@
+import { act } from 'react-dom/test-utils';
+import {v4 as uuidv4} from 'uuid';
 const initialState = {
-    todos:['hello lets start']
+    todos: [
+        {
+            title: 'Lets start',
+            status: false,
+            id:uuidv4()
+         }
+    ]
 }
 
-function todoReducer (state = initialState, action){
-    if(action.type === "ADDTODO"){
-        return ({...state, todos:[...state.todos, action.payload]})
+function todoReducer(state = initialState, action) {
+    if (action.type === "ADDTODO") {
+        
+        return ({
+            ...state,
+            todos: [...state.todos, {...action.payload}]
+        })
     }
 
-    if(action.type === "DELETETODO"){
+    if (action.type === "DELETETODO") {
         var temp = [...state.todos];
-        temp.splice(action.payload, 1);
-        return ({...state, todos:[...temp]})
+        temp = temp.filter((task)=>{
+            if(task.id === action.payload.id){
+                return false
+            }else{
+                return true
+            }
+        })
+        alert('Deleting task')
+        return ({
+            ...state,
+            todos: [...temp]
+        })
     }
-    if(action.type === "DONETODO"){
-        alert('Task completed');
+    
+    if (action.type === 'TOGGLE_TODO_STATUS'){
+        var tempTodos = [...state.todos];
+        console.log(tempTodos);
+        tempTodos = tempTodos.map((todo)=>{
+            if(todo.id === action.payload.id){
+                todo.status ? alert('tast pending') : alert('task completed');
+                todo.status = !todo.status;
+                return todo
+            }
+            return todo
+        })
+        return ({...state, todos:[...tempTodos]})
     }
-return ({...state})
+
+    return ({
+        ...state
+    })
 }
 
 export default todoReducer
