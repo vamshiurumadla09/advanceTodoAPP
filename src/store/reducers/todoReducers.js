a900c1155fc99c8177ed7689496e2505da86cf18
vamshiurumadla09 <urumadlavamshi@gmail.com>
2023-06-23 10:36:36 +0530

    push pro

--- src/store/reducers/todoReducers.js
+++ src/store/reducers/todoReducers.js
@@ -11,7 +11,6 @@ const initialState = {
 
 function todoReducer(state = initialState, action) {
     if (action.type === "ADDTODO") {
-        
         return ({
             ...state,
             todos: [...state.todos, {...action.payload}]
